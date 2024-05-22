d55e96fa9553429640dc604b8e8793144cfb4d13
phuongh2tnt <166468012+phuongh2tnt@users.noreply.github.com>
2024-05-23 04:43:27 +0700

    Update vit.py

--- vit.py
+++ vit.py
@@ -97,7 +97,10 @@ if __name__ == "__main__":
     # 3. Create a new deep model with pre-trained weights
     import torchvision.models as models
     from torchvision.models import ViT_B_16_Weights
-    model = models.vit_b_16(weights=ViT_B_16_Weights.DEFAULT, num_classes=2)
+    model = models.vit_b_16(weights=ViT_B_16_Weights.DEFAULT, num_classes=1000)
+    #fine tune vit
+    num_classes = 2  # Example: for a dataset with 10 classes
+    model.heads.head = torch.nn.Linear(model.heads.head.in_features, num_classes)
     #3.1. Create a new deep model use timm
     #model=timm.create_model('hrnet_w18', pretrained=True, num_classes=2).to('cuda')
 
